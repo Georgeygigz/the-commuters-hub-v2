33a34512ff07f96512b48cf03880086ba082d780
georgeygigz <georgeymutti@gmail.com>
2020-07-25 22:24:50 +0300

    added specific directories

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,13 @@ docker tag commuters-image 241619/commuters:latest
 docker tag commuters-image 241619/commuters:$SHA
 docker push 241619/commuters:latest
 docker push 241619/commuters:$SHA
-kubectl apply -f k8s
+# kubectl apply -f k8s/deployments
+# kubectl apply -f k8s/ingress
+# kubectl apply -f k8s/pvcs
+# kubectl apply -f k8s/services
+
+kubectl apply -f k8s/prod/deployments
+kubectl apply -f k8s/prod/ingress
+kubectl apply -f k8s/prod/services
+
 kubectl set image deployments/server-deployment server=241619/commuters:$SHA
